0798c52241fa0817db77922238492441126857ad
TranTrungTinLV <74409780+TranTrungTinLV@users.noreply.github.com>
2023-06-22 00:48:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -57,8 +57,7 @@ btnRoll.addEventListener('click', function () {
             document.getElementById(`current--${activePlayer}`).textContent = currentScore;
         } else {
             document.querySelector('body').classList.add('backgournd');
-            // var audio = new Audio('./Untitled.wav');
-            // audio.play();
+          
             setTimeout(function () {
                 document.querySelector('body').classList.remove("backgournd")
             }, 900)
